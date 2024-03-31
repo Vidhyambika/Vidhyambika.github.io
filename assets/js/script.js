1892603bcee6b625ba95503efedb9bfb49724da2
Vidhyambika <63492911+Vidhyambika@users.noreply.github.com>
2024-03-31 22:07:32 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["machine learning", "data science", "artificial intelligence", "computer vision", "natural language processing", "generative ai", "full-stack development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
