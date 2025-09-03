728e79e68d9977182ca3cd31b37c7909955b2c41
Vidhyambika SR <63492911+Vidhyambika@users.noreply.github.com>
2025-09-03 17:56:00 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -213,9 +213,7 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
 <!--Start of Tawk.to Script-->
-<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -225,10 +223,7 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-</script>
 <!--End of Tawk.to Script-->
-// End of Tawk.to Live Chat
-
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
